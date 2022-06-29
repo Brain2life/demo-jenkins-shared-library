9cb9cb3862b4873fe1de91db2984fde92a8d5364
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-06-29 17:47:06 +0600

    [ref]: Add script comments

--- vars/getAllCommits.groovy
+++ vars/getAllCommits.groovy
@@ -1,3 +1,9 @@
+#!/usr/bin/env groovy
+
+/*
+Script returns the list of all git commits in the repository branch
+*/
+
 def call() {    
     def commits = sh (
         script: "git log --format=format:%H",
